54fcb2a44121fe2c4b696e8fbf35c1d209eb2d23
santiago <agromero@itba.edu.ar>
2025-06-20 21:48:24 -0300

    code: started option generation methods

--- example-codes/FullCodeExample.py
+++ example-codes/FullCodeExample.py
@@ -12,16 +12,16 @@ FRONTIER_MODE = 'Open'
 EVOLUTION_MODE = 'Transition'
 
 STATES = {'a': 0, 'b': 1, 'c': 2}
-STATE_COLORS = {
-    0: (255, 0, 0),    
-    1: (0, 0, 0),      
-    2: (0, 255, 0)     
-}
+STATE_COLORS = [
+    (255, 0, 0),
+    (0, 0, 0),      
+    (0, 255, 0)     
+]
 
 def transition_function(cells, row, col):
     neighbor_above = get_cell_value(cells, row - 1, col)
     neighbor_right = get_cell_value(cells, row, col + 1)
-    
+
     state_a = STATES['a']
     state_b = STATES['b']
     state_c = STATES['c']
